17977688e5a0bf87baaffde6aa24c2a1a52472b1
braaAwni <braaberoo95@gmail.com>
2022-03-15 14:06:12 +0200

    handel error

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
-
-
+const router = require('./routes/route')
 const app = express();
 app.use(compression());
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-
-
+app.use(router)
 module.exports = app;
